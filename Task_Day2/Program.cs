70ee41ac17f71fe454946feb84cd4e154fb4569f
yusif.pi <yusif.pi@CANR2-3>
2022-08-25 07:59:05 +0400

    Add project files.

---
+++ Task_Day2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task_Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  //Task 1 :for döngüsü kullanmanızı istiyorum 1. örnek için 
+            //        for(int i=1;i<=100;i++)
+            //        Console.WriteLine($"positive = {i} and negative {-1*i}");
+
+
+
+
+            // //Task 2 : 2) Kullanıcıdan aldığınız metinsel değeri tersten ekrana yazdırınız
+            //        string s=Console.ReadLine();
+            //        for(int i=s.Length-1;i>=0;i--)
+            //        Console.Write(s[i]);
+
+            // // TASK 3 : asal sayı olup olmadığını gönderdiğim tabloya göre ayrıştırınız
+            //       for (int i = 10; i <=20; i++)
+            //         {
+            //         bool t=true;
+            //         for (int j = 2; j <=Math.Sqrt(i); j++)
+            //         {
+            //         if(i%j==0)
+            //         {
+            //             t=false;
+            //             Console.WriteLine($"# {i} esittir {j} * {i/j}");
+            //             break;
+            //         }
+
+            //             }
+            //             if(t) Console.WriteLine($"# {i} asal sayidir");
+            //         }
+
+            ////task 4
+
+            //       Console.WriteLine("Sayi giriniz");
+            //       int n=int.Parse(Console.ReadLine());
+
+            //            int mn=n;
+            //            int  mx=n;
+            //       bool t=true;
+            //       while(t)
+            //       {
+            //           Console.WriteLine("Davam etmek Ucun Y , etmemek ucun ise her hansi bir tusa basiniz");
+            //          string answer=Console.ReadLine();
+            //          if(String.Equals(answer,"Y"))
+            //          {
+            //               Console.WriteLine("Sayi giriniz");
+            //               int m=int.Parse(Console.ReadLine());
+
+            //               if(m>mx) mx=m;
+            //               if(m<mn) mn=m;
+
+            //          }
+            //          else 
+            //          {
+            //              Console.WriteLine(mx-mn);
+            //              break;
+            //          }
+            //       }
+        }
+    }
+}
